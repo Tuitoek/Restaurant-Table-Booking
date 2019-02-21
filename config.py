4470c756622822e9e2717e76654671c97c719e36
Sarah Tuitoek <sjtoek@gmail.com>
2019-02-21 14:33:29 +0300

    Updated config.py

--- config.py
+++ config.py
@@ -1,17 +1,29 @@
 import os
-
 class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY='a77302eea2d2'
 
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 
 class ProdConfig(Config):
-    pass
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
 
 
 class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
     DEBUG = True
 
 config_options = {
